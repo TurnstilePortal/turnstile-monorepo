00b83691ffb23aef1b03f4b45f39e3a30a27ddd3
Tim Tickel <twt@clarified.io>
2025-02-20 18:05:08 +0000

    fix tests

--- scripts/example-commands.sh
+++ scripts/example-commands.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-pnpm tsx examples/src/index.ts deposit-and-claim -k docker/turnstile-sandbox/sandbox-keys.json
+KEYS=docker/turnstile-sandbox/sandbox-keys.json
 
-pnpm tsx examples/src/index.ts shield-tokens -k docker/turnstile-sandbox/sandbox-keys.json
+pnpm tsx examples/src/index.ts deposit-and-claim -k $KEYS
 
-pnpm tsx examples/src/index.ts aztec-transfer-private-verified-id -k docker/turnstile-sandbox/sandbox-keys.json --recipient 0x06f24a5e873e56a5f0a363e5179e6d2f648fb4a1a294b9055c55c0a0594b5957
+pnpm tsx examples/src/index.ts shield-tokens -k $KEYS
 
-pnpm tsx examples/src/index.ts aztec-transfer-private-verified-id -k docker/turnstile-sandbox/sandbox-keys.json --recipient 0x06f24a5e873e56a5f0a363e5179e6d2f648fb4a1a294b9055c55c0a0594b5957
+pnpm tsx examples/src/index.ts aztec-transfer-private-verified-id -k $KEYS --recipient 0x06f24a5e873e56a5f0a363e5179e6d2f648fb4a1a294b9055c55c0a0594b5957
+
+pnpm tsx examples/src/index.ts aztec-transfer-private-verified-id -k $KEYS --recipient 0x06f24a5e873e56a5f0a363e5179e6d2f648fb4a1a294b9055c55c0a0594b5957
+
+
+ pnpm tsx examples/src/index.ts  withdraw-tokens -k $KEYS
